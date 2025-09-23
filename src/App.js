af5bf6c0a406df32b3ec240668c073db2dad9098
abhisheksaini3232 <abhishekbadthunia3232@gmail.com>
2025-09-23 22:05:22 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
 import "./App.css";
-import { useContext } from "react";
-import { TodoContext } from "./context/Todo";
 
 function App() {
-  const todoContext = useContext(TodoContext);
-
-  function handleChange(event) {
-    todoContext.setValue(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    todoContext.setTodo([...todoContext.todo, todoContext.value]);
-    todoContext.setValue("");
-  }
-
-  function RemoveTodo(i) {
-    todoContext.setTodo(todoContext.todo.filter((_, index) => i !== index));
-  }
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="textInput">Enter Text:</label>
-        <input
-          id="textInput"
-          type="text"
-          value={todoContext.value}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <ul>
-        {todoContext.todo.map((data, index) => (
-          <li key={index}>
-            {data} <button onClick={() => RemoveTodo(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  <>
+    <h1>Context API</h1>;
+    <h2>Hello</h2>
+  </>
 }
 
 export default App;
