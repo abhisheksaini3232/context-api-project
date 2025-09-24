6bb1e101f855da6866e25e64c085ad6652a436ea
abhisheksaini3232 <abhishekbadthunia3232@gmail.com>
2025-09-24 09:36:37 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 
 function App() {
-  <>
-    <h1>Context API</h1>;
-    <h2>Hello</h2>
-  </>
+  return (
+    <>
+      <h1>Context API</h1>;<h2>Hello</h2>
+    </>
+  );
 }
-
 export default App;
